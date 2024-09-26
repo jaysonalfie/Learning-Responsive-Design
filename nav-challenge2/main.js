291a791f8b255042cc0391e9c1a97f9fcecf446a
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-09-26 09:34:21 +0300

    minor-changes

--- nav-challenge2/main.js
+++ nav-challenge2/main.js
@@ -3,5 +3,6 @@ const nav = document.querySelector('.nav');
 
 
 navToggle.addEventListener('click', () => {
+    //callback function that adds or removes the class nav--visible
     nav.classList.toggle('nav--visible');
 })
